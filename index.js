beb7d2e70c25c87b9e137809dbf7d16521d8b304
Emil Stenberg <esandin@gmail.com>
2016-10-18 12:58:17 +0200

    handle users

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 module.exports = {
   addType (msg) {
-    if (!msg.ug1Name) {
-      return msg
+    const result = JSON.parse(JSON.stringify(msg))
+    if (result.ugClass === 'user') {
+      result.type = 'USER'
+      return result
+    }
+
+    if (!result.ug1Name) {
+      return result
     }
 
     const isTeacherRegExp = /edu\.courses\.\w{2}\.\w{6}\.\d{5}\.\d\.\bteachers\b/
     const isAssistantsRegExp = /edu\.courses\.\w{2}\.\w{6}\.\d{5}\.\d\.\bassistants\b/
     const isCourseResponsibleRegExp = /edu\.courses\.\w{2}\.\w{6}\.\d{5}\.\d\.\bcourseresponsibles\b/
     const isStudentsRegExp = /ladok2\.kurser.\w{2}\.\w{4}.registrerade_\d{5}\.\d/
-    const result = JSON.parse(JSON.stringify(msg))
     // console.log(result)
-    if (result.ugClass === 'user') {
-      result.type = 'USER'
-    } else if (result.ug1Name.match(isTeacherRegExp)) {
+    if (result.ug1Name.match(isTeacherRegExp)) {
       result.type = 'TEACHERS'
     } else if (result.ug1Name.match(isAssistantsRegExp)) {
       result.type = 'ASSISTANTS'
--- test/index.js
+++ test/index.js
@@ -13,4 +13,5 @@ describe('addType.js', function () {
   it('should add the type assistants', () => messageInspector.addType({ug1Name: 'edu.courses.DD.abcdef.20161.1.assistants'}).type.should.equal('ASSISTANTS'))
   it('should add the type courseresponsibles', () => messageInspector.addType({ug1Name: 'edu.courses.DD.abcdef.20161.1.courseresponsibles'}).type.should.equal('COURSERESPONSIBLES'))
   it('should add the type students', () => messageInspector.addType({ug1Name: 'ladok2.kurser.SD.2230.registrerade_20162.1'}).type.should.equal('STUDENTS'))
+  it('should add the type user', () => messageInspector.addType({ugClass: 'user'}).type.should.equal('USER'))
 })
