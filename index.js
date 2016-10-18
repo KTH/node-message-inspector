f6576702040883b3dfb476ea5d0e042038cb3237
Emil Stenberg <esandin@gmail.com>
2016-10-18 11:46:53 +0200

    use stringify to clone

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ module.exports = {
     const isTeacherRegExp = /edu\.courses\.\w{2}\.\w{6}\.\d{5}\.\d\.\bteachers\b/
     const isAssistantsRegExp = /edu\.courses\.\w{2}\.\w{6}\.\d{5}\.\d\.\bassistants\b/
     const isCourseResponsibleRegExp = /edu\.courses\.\w{2}\.\w{6}\.\d{5}\.\d\.\bcourseresponsibles\b/
-    const result = Object.create(msg)
+    const result = JSON.parse(JSON.stringify(msg))
+    // console.log(result)
     if (result.ug1Name.match(isTeacherRegExp)) {
       result.type = 'TEACHERS'
     } else if (result.ug1Name.match(isAssistantsRegExp)) {
