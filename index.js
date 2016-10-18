0d77ae7bb3fec726ca5b89576c75d0107a434699
Emil Stenberg <esandin@gmail.com>
2016-10-18 12:46:03 +0200

    return if no ug1Name

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 module.exports = {
   addType (msg) {
+    if (!msg.ug1Name) {
+      return msg
+    }
+
     const isTeacherRegExp = /edu\.courses\.\w{2}\.\w{6}\.\d{5}\.\d\.\bteachers\b/
     const isAssistantsRegExp = /edu\.courses\.\w{2}\.\w{6}\.\d{5}\.\d\.\bassistants\b/
     const isCourseResponsibleRegExp = /edu\.courses\.\w{2}\.\w{6}\.\d{5}\.\d\.\bcourseresponsibles\b/
