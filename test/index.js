982de338510e2ed53f6a5d65b3582710ed99a243
Emil Stenberg <emilsten@kth.se>
2017-01-23 16:14:35 +0100

    added pre-push: run tests

--- test/index.js
+++ test/index.js
@@ -4,13 +4,10 @@ var should = require('should')
 const {addDescription, type} = require('../index.js')
 
 describe('addDescription.js', function () {
-  beforeEach(function () {
-    // convertTo2dArray = require('../../../convertTo2dArray')
-  })
   describe('course', () => {
     it('should add the type:course, userType:teachers', () => addDescription({ug1Name: 'edu.courses.DD.abcdef.20161.1.teachers'})._desc.should.deepEqual({type: type.course, userType: type.teachers}))
     it('should add the type assistants', () => addDescription({ug1Name: 'edu.courses.DD.abcdef.20161.1.assistants'})._desc.should.deepEqual({type: type.course, userType: type.assistants}))
-    it('should add the type courseresponsibles', () => addDescription({ug1Name: 'edu.courses.DD.abcdef.20161.1.courseresponsibles'})._desc.should.deepEqual({type: type.course, userType: type.courseresponsibles}))
+    it('should add the type courseresponsibles', () => addDescription({ug1Name: 'edu.courses.DD.abcdef.20161.1.courseresponsible'})._desc.should.deepEqual({type: type.course, userType: type.courseresponsibles}))
     it('should add the type students', () => addDescription({ug1Name: 'ladok2.kurser.SD.2230.registrerade_20162.1'})._desc.should.deepEqual({type: type.course, userType: type.students}))
     it('should add the type omregistrerad', () => addDescription({ug1Name: 'ladok2.kurser.KD.1070.omregistrerade_20171'})._desc.should.deepEqual({type: type.course, userType: type.omregistrerade}))
   })
